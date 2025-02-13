9f41329acf24eb64c9875a5894cf479afe5581ca
Sargeanthost <sargeanthost@gmail.com>
2025-02-12 23:17:26 -0500

    (feat): i think this is better way to save

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,8 @@
 import os
 
-os.environ["POLARS_MAX_THREADS"] = "15"  # Change this based on your number of system threads (logical processors in Windows) (cores*threads per core in Linux)
+os.environ["POLARS_MAX_THREADS"] = (
+    "7"  # Change this based on your number of system threads (logical processors in Windows) (cores*threads per core in Linux)
+)
 
 import polars as pl
 from pathlib import Path
@@ -10,18 +12,30 @@ pl.Config.set_tbl_rows(900)
 
 business_path = Path(__file__).parent / "data" / "yelp_academic_dataset_business.json"
 review_path = Path(__file__).parent / "data" / "yelp_academic_dataset_review.json"
+tip_path = Path(__file__).parent / "data" / "yelp_academic_dataset_tip.json"
 
 task1_business_review_path = Path(__file__).parent / "data" / "task1_business_review.json"
+task1_business_tip_path = Path(__file__).parent / "data" / "task1_business_tip.json"
 
 business_df = pl.scan_ndjson(business_path)
 review_df = pl.scan_ndjson(review_path)
-
+tip_df = pl.scan_ndjson(tip_path)
 
 # Examine data
-print(business_df.filter(pl.col("review_count").ge(35)).group_by(pl.col("state")).len().sort(pl.col("len")).collect(streaming=True))
+print(
+    business_df.filter(pl.col("review_count").ge(55))
+    .group_by(pl.col("state"))
+    .len()
+    .sort(pl.col("len"))
+    .collect(streaming=True)
+)
 
-# Get just PA business that have  or more reviews
-business_df.filter(pl.col("state").eq("PA") & pl.col("review_count").ge(35)).join(review_df, on="business_id").unique().collect(streaming=True).write_ndjson(task1_business_review_path)
+# Filter data
+business_df = business_df.filter(pl.col("state").eq("PA") & pl.col("review_count").ge(55))
+review_df = review_df.filter(pl.col("text").str.len_chars().ge(25))
+tip_df = tip_df.filter(pl.col("text").str.len_chars().get(5))
 
-# Cant merge tip into business and review df since each businesses' tips would be be a column per tip per review, and take up massive amounts of memory.
-# Can make business and tip df if needed
+# write out . need to check if collecting here is fine.
+business_df.join(review_df, on="business_id").unique().collect(streaming=True).write_parquet(task1_business_review_path)
+
+business_df.join(tip_df, on="business_id").unique().collect(streaming=True).write_parquet(task1_business_tip_path)
